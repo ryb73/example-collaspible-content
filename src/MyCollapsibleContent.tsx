f2cb19b7fc6c0d4b81067b79d16487924521489e
Ryan <rpbiwer@gmail.com>
2023-07-12 17:49:17 -0400

    Initial commit

---
+++ src/MyCollapsibleContent.tsx
@@ -0,0 +1,9 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
+
+export function CustomContent() {
+    return (
+      <Collapsible.Content>
+        Content
+      </Collapsible.Content>
+    );
+  }
