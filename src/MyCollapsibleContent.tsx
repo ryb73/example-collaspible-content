f814a70dc4d3ff1c8b0b72c8e54089e2c2d7b46b
Ryan <rpbiwer@gmail.com>
2023-07-12 18:14:42 -0400

    Format

--- src/MyCollapsibleContent.tsx
+++ src/MyCollapsibleContent.tsx
@@ -1,9 +1,5 @@
 import * as Collapsible from "@radix-ui/react-collapsible";
 
 export function CustomContent() {
-    return (
-      <Collapsible.Content>
-        Content
-      </Collapsible.Content>
-    );
-  }
+  return <Collapsible.Content>Content</Collapsible.Content>;
+}
